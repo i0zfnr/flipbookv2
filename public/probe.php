77e9a7eb39005a65f6edaa9f336f85e8123377ed
i0zfnr <irfanhafizul123@gmail.com>
2026-09-14 11:58:58 +0800

    Probe 8001 body

--- public/probe.php
+++ public/probe.php
@@ -1,24 +1,6 @@
 ﻿<?php
-$data = [];
-
-function get_http($port, $path = '/') {
-    $ch = curl_init("http://127.0.0.1:$port$path");
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_TIMEOUT, 3);
-    curl_setopt($ch, CURLOPT_HEADER, true);
-    $res = curl_exec($ch);
-    $info = curl_getinfo($ch);
-    $err = curl_error($ch);
-    curl_close($ch);
-    return [
-        'http_code' => $info['http_code'],
-        'error' => $err,
-        'response' => substr($res, 0, 500)
-    ];
-}
-
-$data['http_8000'] = get_http(8000);
-$data['http_8001'] = get_http(8001);
-$data['http_9000'] = get_http(9000);
-
-echo json_encode($data, JSON_PRETTY_PRINT);
+$ch = curl_init("http://127.0.0.1:8001/");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$res = curl_exec($ch);
+curl_close($ch);
+echo substr($res, 0, 1000);
