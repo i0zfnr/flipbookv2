3d0df64f8480dfed41d1a7d9b2c9db065c6b50a7
i0zfnr <irfanhafizul123@gmail.com>
2026-09-14 12:00:43 +0800

    Test shell_exec in probe.php

--- public/probe.php
+++ public/probe.php
@@ -1,6 +1,12 @@
 ﻿<?php
-$ch = curl_init("http://127.0.0.1:8001/");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$res = curl_exec($ch);
-curl_close($ch);
-echo substr($res, 0, 1000);
+$data = [];
+$data['shell_exec_exists'] = function_exists('shell_exec');
+$data['disabled_functions'] = ini_get('disable_functions');
+
+if (function_exists('shell_exec')) {
+    $data['ps'] = shell_exec('ps aux');
+    $data['netstat'] = shell_exec('netstat -tlpn 2>&1 || ss -tlpn 2>&1');
+    $data['hostname'] = shell_exec('hostname');
+}
+
+echo json_encode($data, JSON_PRETTY_PRINT);
